6385e6f3763d904218132d39ec4aacd755e96115
ElliotCleary <55023402+Elliothc13@users.noreply.github.com>
2023-02-19 21:02:16 +0000

    feat(interceptor): :sparkles: added some code to ContextObject

--- src/main/ContextObject.java
+++ src/main/ContextObject.java
@@ -1,5 +1,27 @@
 package main;
 
 public class ContextObject {
+    private Rental rental;
+    private double amount;
 
+    public ContextObject(Rental rental) {
+        this.rental = rental;
+        this.amount = rental.getCharge();
+    }
+
+    public Rental getRental() {
+        return rental;
+    }
+
+    public int getDaysRented() {
+        return rental.getDaysRented();
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
